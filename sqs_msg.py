3eba2fc37b725007b809e6fd9e3e9e854258014e
Joseph Peter <josephvpeter7@gmail.com>
2022-11-23 05:02:03 -0500

    notes added in lambda_fn_sqs_dyn

--- sqs_msg.py
+++ sqs_msg.py
@@ -4,4 +4,4 @@ import boto3
 
 sqs = boto3.resource('sqs') # create service request objects
 queue = sqs.create_queue(QueueName='Project-Week-15-TestQueue') # Create the SQS queue
-print(queue.url) # url for msg
+print('Queue URL',queue.url) # url for cloudwatch logs
